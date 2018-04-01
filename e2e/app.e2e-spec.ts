0cbc9f6418f69255198d0506a622214bb6fe258e
glyder <georgepagotelis@hotmail.com>
2018-04-01 17:02:03 +1000

    first commit

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AppPage } from './app.po';
+
+describe('angular5-coursetro2-ngmaterial App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
